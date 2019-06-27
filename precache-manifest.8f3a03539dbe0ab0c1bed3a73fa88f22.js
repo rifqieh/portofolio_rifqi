c45997645c50ab347ed87d42a4181a43db8256b2
rifqieka <ekarifqi@ymail.com>
2019-06-28 00:38:00 +0700

    Updates

---
+++ precache-manifest.8f3a03539dbe0ab0c1bed3a73fa88f22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "049f3860b551d44a8c13dcfceed1783a",
+    "url": "/portofolio_rifqi/index.html"
+  },
+  {
+    "revision": "5c34c6a37bed51e28789",
+    "url": "/portofolio_rifqi/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3aabe6b09e3d35da9d70",
+    "url": "/portofolio_rifqi/static/js/2.35550e1d.chunk.js"
+  },
+  {
+    "revision": "5c34c6a37bed51e28789",
+    "url": "/portofolio_rifqi/static/js/main.0a2d2a1f.chunk.js"
+  },
+  {
+    "revision": "0ff3d5be741097302a27",
+    "url": "/portofolio_rifqi/static/js/runtime~main.d04d53cd.js"
+  }
+]);
